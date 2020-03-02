59bcd12f5fc4900edf0428714e6b5c1a30ed0ba3
fbennets <bennet_s@protonmail.com>
2020-03-02 00:38:46 +0000

    Fixed typo

--- opendecision/settings.py
+++ opendecision/settings.py
@@ -77,7 +77,7 @@ if os.environ.get('DJANGO_PRODUCTION') is not None:
         'allauth',
         'allauth.account',
         'allauth.socialaccount',
-        'django_inlinecss'
+        'django_inlinecss',
 
         'users',
         'interpreter',
