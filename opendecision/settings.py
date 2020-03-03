a6b9245518dd5a12fd03fb7696f29c9925955780
fbennets <bennet_s@protonmail.com>
2020-03-03 18:09:59 +0000

    Fixes databases for heroku

--- opendecision/settings.py
+++ opendecision/settings.py
@@ -19,7 +19,13 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 if os.environ.get('HEROKU') is not None:
     ALLOWED_HOSTS = ['.herokuapp.com']
-
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
     # Heroku: Update database configuration from $DATABASE_URL.
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
