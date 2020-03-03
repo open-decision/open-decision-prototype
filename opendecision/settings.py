288af16eabe5c89759186edd54a24e7bfd0c7c5c
fbennets <bennet_s@protonmail.com>
2020-03-03 15:58:15 +0000

    Removed whitenoise middleware for Azure

--- opendecision/settings.py
+++ opendecision/settings.py
@@ -55,7 +55,6 @@ elif os.environ.get('AZURE') is not None:
 
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
-        'whitenoise.middleware.WhiteNoiseMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
